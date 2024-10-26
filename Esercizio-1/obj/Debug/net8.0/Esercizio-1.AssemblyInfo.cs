8d70e5052d1646943ab4d81da76723885f3731a3
marcoColautti <marco.colautti@hotmail.it>
2024-10-26 23:16:24 +0200

    UPD - 25_Oct_2024

--- Esercizio-1/obj/Debug/net8.0/Esercizio-1.AssemblyInfo.cs
+++ Esercizio-1/obj/Debug/net8.0/Esercizio-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Esercizio-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8126c7124272ae48f95d213571bab9def498ca7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Esercizio-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Esercizio-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
